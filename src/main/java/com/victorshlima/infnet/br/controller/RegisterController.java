1c63a37a2ac7ed635e7e92bddff2e90517d3027c
Victor Hugo <victorshlima@gmail.com>
2022-04-11 23:39:10 -0300

    rest tempalte,  controler log

--- src/main/java/com/victorshlima/infnet/br/controller/RegisterController.java
+++ src/main/java/com/victorshlima/infnet/br/controller/RegisterController.java
@@ -2,18 +2,19 @@ package com.victorshlima.infnet.br.controller;
 
 import com.victorshlima.infnet.br.domain.dto.UserDto;
 import com.victorshlima.infnet.br.service.RegisterService;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
-
+@Slf4j
 @RestController
 public class RegisterController {
 
-    private final RegisterService registerService;
+    @Autowired
+    private RegisterService registerService;
 
     public RegisterController (RegisterService registerService){
     this.registerService = registerService;
@@ -21,6 +22,7 @@ public class RegisterController {
 
     @GetMapping(value ="/user/{id}")
     public ResponseEntity<UserDto> getUser(@PathVariable("id") Long id){
+        log.info("getUser");
         var user =  registerService.findUser(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
@@ -29,6 +31,7 @@ public class RegisterController {
     public ResponseEntity<Long> deleteUser(@PathVariable("id") Long id){
         //Criar serviço
         var randon = new Random();
+        log.info("deleteUser");
        // melhor lançar uma exception do que essa ganbiarra
         if (randon.nextBoolean()) return new ResponseEntity<>( id, HttpStatus.OK);
         else return new ResponseEntity<>(id, HttpStatus.NOT_FOUND);
@@ -37,6 +40,7 @@ public class RegisterController {
     @PostMapping("/user")
     public ResponseEntity<UserDto> userRegister(@RequestBody UserDto user){
         //TODO remover a necessidade do envio do ID
+        log.info("userRegister");
         registerService.saveUser(user);
      return new ResponseEntity<UserDto>(user, HttpStatus.CREATED);
     }
